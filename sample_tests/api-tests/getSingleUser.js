56d7493030976801735c780f08db7c6fa59bf2b9
pedromaresteves@gmail.com <pmarques@idealista.com>
2022-10-24 14:52:58 +0200

    Remove console log

--- sample_tests/api-tests/getSingleUser.js
+++ sample_tests/api-tests/getSingleUser.js
@@ -5,7 +5,6 @@ const {assert} = require('chai');
 module.exports = {
     'Test single User Endpoint': async function(){
         const res = await axios.get(`${globals.apiTestingBaseUrl}api/user/2`);
-        console.log(res.data)
         assert.equal(res.status, 200);
         assert.equal(res.data.data.id, 2);
         assert.exists(res.data.data);
